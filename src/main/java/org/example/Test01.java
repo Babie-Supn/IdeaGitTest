a53c530c99b2d115c8c07e31b14a7a3fc0d02552
Barbie <3141730662@qq.com>
2023-09-13 21:13:47 +0800

    Main-commit2

--- src/main/java/org/example/Test01.java
+++ src/main/java/org/example/Test01.java
@@ -4,6 +4,9 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("这是一行测试代码");
         System.out.println("这是main分支添加的一行代码");
+        System.out.println("第一次推送");
+        int n=100000;
+        System.out.println(n);
 
     }
 }
